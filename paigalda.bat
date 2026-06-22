46ae64082e380ed1bc9444f18f1ce9c36ac9b516
KarlEMa-lang <superkalle333@gmail.com>
2026-06-22 12:39:02 +0300

    Delete paigalda.bat

--- paigalda.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Paigaldan vajalikud teegid...
-pip install requests beautifulsoup4
-echo.
-echo Valmis! Nüüd:
-echo 1. Seadista config.json (bot_token, filtrid)
-echo 2. Käivita: python seadista_bot.py  (chat_id saamiseks)
-echo 3. Käivita: python kv_monitor.py
-pause
